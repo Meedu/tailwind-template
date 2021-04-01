004547c88e1be9bc8806fe6c593b7f6f97c5fce1
小滕 <xiaoteng@192.168.0.103>
2021-04-01 14:35:38 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,10 @@
 const { src, dest, series, parallel } = require('gulp');
 const clean = require('gulp-clean');
-const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss')
 const sourcemaps = require('gulp-sourcemaps')
 
 function pcClear() {
     return src('dist/pc', { read: false, allowEmpty: true })
-        .pipe(clean());
-}
-function h5Clear() {
-    return src('dist/h5', { read: false, allowEmpty: true })
         .pipe(clean());
 }
 
@@ -24,30 +19,14 @@ function pcCss() {
         .pipe(sourcemaps.write('.'))
         .pipe(dest('dist/pc/'))
 }
-function h5Css() {
-    return src('src/h5/app.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([
-            require('tailwindcss'),
-            require('autoprefixer'),
-            // require('postcss-csso'),
-        ]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('dist/h5/'))
-}
+
 
 function pcJs() {
     return src('src/pc/app.js')
-        .pipe(uglify())
+        // .pipe(uglify())
         .pipe(dest('dist/pc/'));
 }
 
-function h5Js() {
-    return src('src/h5/app.js')
-        .pipe(uglify())
-        .pipe(dest('dist/h5'));
-}
 
 exports.pc = series(pcClear, parallel(pcJs, pcCss))
-exports.h5 = series(h5Clear, parallel(h5Css, h5Js))
-exports.default = parallel(series(pcClear, parallel(pcJs, pcCss)), series(h5Clear, parallel(h5Css, h5Js)))
+exports.default = series(pcClear, parallel(pcJs, pcCss))
